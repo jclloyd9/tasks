0c0d71afaead24d7f7af589c45180d7822d2c3e8
JAMES <jclloyd@udel.edu>
2023-09-27 20:16:08 -0400

    feeling: goated

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,17 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
-import { Button, Col, Container, Row } from "react-bootstrap";
-import goated from "./waitimgoated.png";
-=======
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
-
->>>>>>> upstream/task-state
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-<<<<<<< HEAD
-            <Container>
-                <Row>
-                    <Col>
-                        <h1>James Lloyd</h1>
-                        <p>Hello World!</p>
-                        <div>
-                            Best Video Games of All Time:
-                            <ol>
-                                <li>Mario Kart Wii</li>
-                                <li>Prime Fortnite</li>
-                                <li>Rocket League</li>
-                            </ol>
-                        </div>
-                        <Button onClick={() => console.log("Hello World!")}>
-                            Log Hello World
-                        </Button>
-                        <div className="rectangle"></div>
-                    </Col>
-                    <Col>
-                        <div className="rectangle"></div>
-                        <span></span>
-                        <img src={goated} alt="wait im goated" />
-                    </Col>
-                </Row>
-            </Container>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -57,7 +24,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,47 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempt, setAttempt] = useState<number>(4);
+    const [inProgress, changeProgress] = useState<boolean>(false);
+
+    function startQuiz(): void {
+        setAttempt(attempt - 1);
+        changeProgress(!inProgress);
+    }
+
+    function flipProgress() {
+        changeProgress(false);
+    }
+
+    function mulligan(): void {
+        setAttempt(attempt + 1);
+    }
+
+    /*function checkZero(): boolean {
+        if (attempt === 0) {
+            return true;
+        } else {
+            return false;
+        }
+        () => changeProgress(true)
+    }*/
+
+    return (
+        <span>
+            <Button
+                onClick={startQuiz}
+                disabled={inProgress === true || attempt === 0}
+            >
+                Start Quiz
+            </Button>
+            {inProgress === false}
+            <Button onClick={flipProgress} disabled={inProgress === false}>
+                Stop Quiz
+            </Button>
+            <Button onClick={mulligan} disabled={inProgress === true}>
+                Mulligan
+            </Button>
+            {attempt}
+        </span>
+    );
 }
